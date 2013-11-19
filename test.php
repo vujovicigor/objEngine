ff8afe8f6ccacd6fb882820eb7d434855e13fe14
Igor Vujovic <vujovichigor@gmail.com>
2013-11-19 14:55:39 +0100

    test file

---
+++ test.php
@@ -0,0 +1,20 @@
+<?PHP
+  require_once($_SERVER["DOCUMENT_ROOT"].'/objengine/buildhtmlclient.php'); 
+  $content = file_get_contents(__FILE__);
+  $strToParse = startParse(substr($content, strpos($content,'?'.'>')+2));
+  //$strToParse = startParse($strToParse);
+  //eval(' ?'.'>'.$strToParse);
+  echo($strToParse);
+  exit(); 
+?>		
+
+      <!--FETCH.Reklama -->
+<h2>$Reklama.naslov</h2> 
+<img src="objengine/image.php?filename=$Reklama.slika&w=100">
+<h2>$Reklama.datum</h2> 
+
+
+
+     <!--ENDFETCH.Reklama -->	
+
+
